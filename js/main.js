69e05338b6afe62119f38ee62001e0f5970ae0e9
tiagoluis86 <70555750+tiagoluis86@users.noreply.github.com>
2021-12-29 10:50:17 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -41,3 +41,16 @@ var color = currentNumber;
     }
 
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.background = "DarkSeaGreen";
+  }); 
+
+  function random_bg_color() {
+    var x = Math. floor(Math. random() * 256);
+    var y = Math. floor(Math. random() * 256);
+    var z = Math. floor(Math. random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console. log(bgColor);
+    document. body. style. background = bgColor;
+}
